cf65991f344ae365f72145f055cfe86dfb445775
Sergey Sudakov <s3gaddtv@gmail.com>
2021-02-06 21:32:01 +0300

    feat: create error notification comp

---
+++ src/components/ErrorNotification/ErrorNotification.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './ErrorNotification.scss';
+
+const ErrorNotification = () => (
+    <div className="notification">
+        Ошибка при загрузке данных
+    </div>
+);
+
+export default ErrorNotification;
