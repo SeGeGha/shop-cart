0fa0f4ff84f662933496858cf4948b3c680f03b8
Sergey Sudakov <s3gaddtv@gmail.com>
2021-02-07 19:48:22 +0300

    feat: add cost-ceil highlight

--- src/components/ProductTable/ProductItem/ProductItem.jsx
+++ src/components/ProductTable/ProductItem/ProductItem.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
-const ProductItem = ({ info, id }) => (
-    <tr className="product-table_row" id={id}>
-        <td className="product-table_row_cell">
-            {info.N}
-            (
-            {info.P}
-            )
-        </td>
-        <td className="product-table_row_cell">
-            {info.C}
-        </td>
-    </tr>
-);
+function usePrevious(value) {
+    const ref = useRef();
+    useEffect(() => {
+        ref.current = value;
+    });
+    return ref.current;
+}
+
+const ProductItem = ({ info, id }) => {
+    const prevProductCost = usePrevious(info.C);
+    const ceilHighlight = (info.C > prevProductCost) ? 'red' : 'green';
+
+    return (
+        <tr className="product-table_row" id={id}>
+            <td className="product-table_row_cell">
+                {info.N}
+                (
+                {info.P}
+                )
+            </td>
+            <td className={`product-table_row_cell product-table_row_cell--${ceilHighlight}-highlight`}>
+                {info.C}
+            </td>
+        </tr>
+    );
+};
 
 export default ProductItem;
